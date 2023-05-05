ea54f7884e28b9135dab735814eeea877a04402f
hovelopin <kimhojin3714@naver.com>
2023-05-05 11:18:43 +0900

    boards 모듈 생성 및 불필요한 파일 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { BoardsModule } from './boards/boards.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  // 명령어를 통해서 Board 모듈을 넣으면 알아서 import가 App모듈에 들어왔다.
+  imports: [BoardsModule],
 })
 export class AppModule {}
