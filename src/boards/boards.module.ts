77a3eeadd5fb95c2ea07117fd5be2b70b6061537
hovelopin <kimhojin3714@naver.com>
2023-05-05 12:13:43 +0900

    controller , service , module 명령어로 만들기

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { BoardsController } from './boards.controller';
+import { BoardsService } from './boards.service';
 
-@Module({})
+@Module({
+  // 명령어를 입력하면 Module에 Controller를 연결시켜준다.
+  controllers: [BoardsController],
+  providers: [BoardsService],
+})
 export class BoardsModule {}
