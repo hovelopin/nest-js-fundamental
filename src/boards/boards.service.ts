1ca9d1e5e3e24cbe8aac4ba42f2b3c88f6715b32
hovelopin <kimhojin3714@naver.com>
2023-05-06 13:16:26 +0900

    모든 게시물 불러오는 Service 로직 작성

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { Board } from './board.model';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+  // private를 사용한 이유는 다른 컴포넌트에서 수정을 불가능하기 위해서다.
+  private boards: Board[] = [];
+
+  // 배열에 있는 모든 값을 받을 수 있다.
+  getAllBoards() {
+    return this.boards;
+  }
+}
