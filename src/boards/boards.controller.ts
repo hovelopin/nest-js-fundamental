3f033326047bb0c2aeb463b8d6efd1328064bdce
hovelopin <kimhojin3714@naver.com>
2023-05-13 16:45:12 +0900

    게시물 생성 Controller 정의

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 
 @Controller('boards')
@@ -8,5 +8,15 @@ export class BoardsController {
   @Get('/')
   getAllBoard() {
     return this.boardsService.getAllBoards();
+  }
+
+  // NestJS에서 클라이언트가 보낸 body값을 확인하기 위해서는 아래와 같인 @Body() body 해주면 된다.
+  // 하나만 갖고 싶을때는 @Body('title') title 이렇게 가져올 수 있다.
+  @Post('/')
+  createBoard(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ) {
+    return this.boardsService.creatBoard(title, description);
   }
 }
